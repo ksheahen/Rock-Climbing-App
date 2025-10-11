7053a655e1aa0dbdb6b26211c309035763f290ac
Tate, Thane <thanetate@my.unt.edu>
2025-10-11 11:11:12 -0500

    Init Category Component

--- app/(components)/category.tsx
+++ app/(components)/category.tsx
@@ -1,11 +1,23 @@
-import { Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
+import { Text, View } from "react-native";
 
 function CategoryComponent() {
-    return (
-        <>
-            <Text>Category Component</Text>
-        </>
-    );
-};
+	const [selectedLanguage, setSelectedLanguage] = useState();
+
+	return (
+		<View className="category-component-container">
+			<Text className="title">Category</Text>
+			<Picker
+				selectedValue={selectedLanguage}
+				onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
+			>
+				<Picker.Item label="Gym" value="Gym" />
+				<Picker.Item label="Board" value="Board" />
+				<Picker.Item label="Outdoors" value="Outdoors" />
+			</Picker>
+		</View>
+	);
+}
 
 export default CategoryComponent;
