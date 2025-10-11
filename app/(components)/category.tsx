21cddabb741e998a121769853d4013fec9db2b75
Tate, Thane <thanetate@my.unt.edu>
2025-10-11 11:54:15 -0500

    Global Styling

--- app/(components)/category.tsx
+++ app/(components)/category.tsx
@@ -1,21 +1,27 @@
-import { Picker } from "@react-native-picker/picker";
-import { useState } from "react";
 import { Text, View } from "react-native";
+import Icon from "react-native-remix-icon";
+import { styles } from "../styles/category";
 
 function CategoryComponent() {
-	const [selectedLanguage, setSelectedLanguage] = useState();
+	// const [selectedLanguage, setSelectedLanguage] = useState();
 
 	return (
-		<View className="category-component-container">
-			<Text className="title">Category</Text>
-			<Picker
+		<View style={styles.container}>
+			<Text style={styles.title}>Category</Text>
+			<Text style={styles.dropdown}>Gym</Text>
+			<View style={styles.icon_container}>
+				<Icon name="arrow-drop-down-line"></Icon>
+			</View>
+
+			{/* For now we wont implement the picker */}
+			{/* <Picker
 				selectedValue={selectedLanguage}
 				onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
 			>
 				<Picker.Item label="Gym" value="Gym" />
 				<Picker.Item label="Board" value="Board" />
 				<Picker.Item label="Outdoors" value="Outdoors" />
-			</Picker>
+			</Picker> */}
 		</View>
 	);
 }
---
+++ app/styles/category.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+import { FONT_SIZES } from "./global-styles";
+
+export const styles = StyleSheet.create({
+  container: {
+    borderColor: "red",
+    borderStyle: "solid",
+    width: "auto",
+    height: "auto",
+  },
+  title: {
+    fontSize: FONT_SIZES.small,
+
+  },
+  dropdown: {
+
+  },
+  icon_container: {
+
+  },
+});
