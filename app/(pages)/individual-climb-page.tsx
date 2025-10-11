e0cf0abf39290ca96ef745b86df40adcce0a6177
Tate, Thane <thanetate@my.unt.edu>
2025-10-11 12:23:43 -0500

    Line Component

--- app/(pages)/individual-climb-page.tsx
+++ app/(pages)/individual-climb-page.tsx
@@ -1,11 +1,13 @@
 import { Text } from "react-native";
 import CategoryComponent from "../(components)/category";
+import LineComponent from "../(components)/line";
 
 function IndividualClimbPage () {
     return (
         <>
             <Text>individual climb page</Text>
             <CategoryComponent/>
+            <LineComponent/>
         </>
     )
 }
