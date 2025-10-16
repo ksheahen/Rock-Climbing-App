b106405f8ca080ece1ff97299ca5f370db917c2d
Kathryn <112595660+ksheahen@users.noreply.github.com>
2025-10-16 09:43:16 -0500

    Updated linting config to include backend folders

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -7,7 +7,7 @@ module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    files: ["**/*.{js,jsx,ts,tsx"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     ignores: ["dist/*", "node_modules/*"],
   },
 ]);
