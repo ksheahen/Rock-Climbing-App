4f0fb8f439c6e88d4b41fd6404d7de403a518fde
AymenBeshir <aymenaaa077@gmail.com>
2025-10-30 14:58:44 -0500

    Implement Picker for Type Component

--- app/(components)/type.tsx
+++ app/(components)/type.tsx
@@ -1,31 +1,63 @@
-import { Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useEffect, useState } from "react";
+import { Pressable, Text, View } from "react-native";
 import Icon from "react-native-remix-icon";
 import styles from "../styles/type";
 
-function TypeComponent() {
-  // const [selectedLanguage, setSelectedLanguage] = useState();
+interface TypeComponentProps {
+  selectedCategoryProp: string;
+  onSelectedCategoryChange?: (category: string) => void;
+}
+
+function TypeComponent({
+  selectedCategoryProp,
+  onSelectedCategoryChange,
+}: TypeComponentProps) {
+  const [selectedType, setSelectedType] = useState(
+    selectedCategoryProp || "Boulder"
+  );
+  const [isPickerVisible, setIsPickerVisible] = useState(false);
+
+  // notify parent whenever the state changes
+  useEffect(() => {
+    if (onSelectedCategoryChange) {
+      onSelectedCategoryChange(selectedType);
+    }
+  }, [selectedType, onSelectedCategoryChange]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Type</Text>
-      <View style={styles.dropdown_container}>
-        <Text style={styles.dropdown}>Boulder</Text>
-        <View style={styles.icon_container}>
-          <Icon name="arrow-drop-down-line" size="24"></Icon>
-        </View>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Type</Text>
+        <Pressable
+          style={styles.dropdown_container}
+          onPress={() => setIsPickerVisible(!isPickerVisible)}
+        >
+          <Text style={styles.dropdown}>{selectedType}</Text>
+          <View style={styles.icon_container}>
+            <Icon name="arrow-drop-down-line" size="24"></Icon>
+          </View>
+        </Pressable>
       </View>
 
-      {/* TODO: implement the picker */}
-      {/* <Picker
-				selectedValue={selectedLanguage}
-				onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
-			>
-				<Picker.Item label="Gym" value="Gym" />
-				<Picker.Item label="Board" value="Board" />
-				<Picker.Item label="Outdoors" value="Outdoors" />
-			</Picker> */}
+      {/* conditionally render the picker */}
+      {isPickerVisible && (
+        <View style={styles.picker_container}>
+          <Picker
+            selectedValue={selectedType}
+            onValueChange={(e) => setSelectedType(e)}
+            itemStyle={styles.picker}
+          >
+            <Picker.Item label="Boulder" value="Boulder" />
+            <Picker.Item label="Top-Rope" value="Top-Rope" />
+            <Picker.Item label="Lead" value="Lead" />
+            <Picker.Item label="Auto-Belay" value="Auto-Belay" />
+          </Picker>
+        </View>
+      )}
     </View>
   );
 }
 
 export default TypeComponent;
+
