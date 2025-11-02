c0c728ed8e10aae81464149e599beab27b615090
Kathryn <112595660+ksheahen@users.noreply.github.com>
2025-11-02 10:18:03 -0600

    configured jest for frontend testing and added login page tests

---
+++ jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: "jest-expo",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["<rootDir>/app/**/*.(spec|test).(ts|tsx|js|jsx)"],
+};
