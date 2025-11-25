14a8a27fe5e8514a223d249c690eb6a0d0d368bf
Tate, Thane <thanetate@my.unt.edu>
2025-11-25 14:06:11 -0600

    Log Page

--- components/Header/Header.styles.ts
+++ components/Header/Header.styles.ts
@@ -2,23 +2,21 @@ import { global } from "@/theme";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    marginTop: global.margin.page_border * 4,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: global.margin.page_border,
-    paddingVertical: 10,
-    backgroundColor: global.colors.background_1,
-  },
-  button: {
-    backgroundColor: "#E5E5EA",
-    borderRadius: global.border.border_radius,
-    paddingHorizontal: 12,
-    paddingVertical: 5,
-  },
-  buttonText: {
-    color: "#000",
-    fontSize: 15,
-  },
+	container: {
+		marginTop: global.margin.page_top,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		backgroundColor: global.colors.background_1,
+	},
+	button: {
+		backgroundColor: global.colors.background_3,
+		borderRadius: global.border.border_radius,
+		paddingHorizontal: 12,
+		paddingVertical: 5,
+	},
+	buttonText: {
+		color: global.colors.text_1,
+		fontSize: global.font_size.phone_medium,
+	},
 });
