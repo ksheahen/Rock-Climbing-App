fee21943646127e4c75b3bafd50e78bf413c0cdd
AymenBeshir <aymenaaa077@gmail.com>
2025-11-28 19:57:11 -0600

    Code clean up

--- jest.config 2.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "jest-expo",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testMatch: ["<rootDir>/app/**/*.(spec|test).(ts|tsx|js|jsx)"],
-  setupFiles: ["<rootDir>/jest.setup.cjs"],
-};
