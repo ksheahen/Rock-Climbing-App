71f554cb59c8b91042fde2db39786ea8d5edc639
Kathryn <112595660+ksheahen@users.noreply.github.com>
2025-11-29 12:33:55 -0600

    error handling and styling for login/sign up pages updated

--- components/Email/Email.tsx
+++ components/Email/Email.tsx
@@ -1,22 +1,24 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { TextInput, View } from "react-native";
-import { styles } from "./Email.styles";
+import colors from "../../theme/index";
+import styles from "../../app/styles/login.styles";
 
 interface EmailProps {
   email: string;
   setEmail: (text: string) => void;
+  inputStyle?: object;
 }
 
-function EmailComponent({ email, setEmail }: EmailProps) {
+function EmailComponent({ email, setEmail, inputStyle }: EmailProps) {
   return (
     <View style={styles.emailContainer}>
       <MaterialIcons name="email" size={20} />
       <TextInput
-        style={styles.input}
+        style={[styles.input, inputStyle]}
         onChangeText={(text) => setEmail(text)}
         value={email}
         placeholder="Email Address"
-        placeholderTextColor={global.colors.text_2}
+        placeholderTextColor={colors.colors.text_2}
         autoCapitalize={"none"}
         autoComplete={"email"}
       />
