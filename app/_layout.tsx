3326bbdd6a3736dad8e4853e042b30c20e3dace4
Tate, Thane <thanetate@my.unt.edu>
2026-02-03 18:29:12 -0600

    Fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,10 +10,11 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 function RootLayout() {
 	return (
-		<SQLiteProvider
-			databaseName="climb.db"
-			onInit={async (db) => {
-				await db.execAsync(`
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<SQLiteProvider
+				databaseName="climb.db"
+				onInit={async (db) => {
+					await db.execAsync(`
         CREATE TABLE IF NOT EXISTS log_climb5 (
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
         uuid TEXT UNIQUE,
@@ -32,35 +33,36 @@ function RootLayout() {
         );
         PRAGMA journal_mode=WAL;
         `);
-				// Check existing columns
-				const columns = await db.getAllAsync("PRAGMA table_info(log_climb3);");
-				const existingCols = columns.map((c: any) => c.name);
+					// Check existing columns
+					const columns = await db.getAllAsync("PRAGMA table_info(log_climb3);");
+					const existingCols = columns.map((c: any) => c.name);
 
-				// Add uuid column if missing (no UNIQUE here)
-				if (!existingCols.includes("uuid")) {
-					await db.execAsync(`ALTER TABLE log_climb3 ADD COLUMN uuid TEXT;`);
-				}
+					// Add uuid column if missing (no UNIQUE here)
+					if (!existingCols.includes("uuid")) {
+						await db.execAsync(`ALTER TABLE log_climb3 ADD COLUMN uuid TEXT;`);
+					}
 
-				// Add deleted column if missing
-				if (!existingCols.includes("deleted")) {
-					await db.execAsync(`ALTER TABLE log_climb3 ADD COLUMN deleted INTEGER DEFAULT 0;`);
-				}
+					// Add deleted column if missing
+					if (!existingCols.includes("deleted")) {
+						await db.execAsync(`ALTER TABLE log_climb3 ADD COLUMN deleted INTEGER DEFAULT 0;`);
+					}
 
-				// Add synced column if missing
-				if (!existingCols.includes("synced")) {
-					await db.execAsync(`ALTER TABLE log_climb3 ADD COLUMN synced INTEGER DEFAULT 0;`);
-				}
-			}}
+					// Add synced column if missing
+					if (!existingCols.includes("synced")) {
+						await db.execAsync(`ALTER TABLE log_climb3 ADD COLUMN synced INTEGER DEFAULT 0;`);
+					}
+				}}
 
 
-		>
+			>
 
-			{/* this makes apple's status bar black */}
-			<StatusBar barStyle="dark-content" />
-			<Stack screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="(pages)" options={{ headerShown: false }} />
-			</Stack>
-		</SQLiteProvider>
+				{/* this makes apple's status bar black */}
+				<StatusBar barStyle="dark-content" />
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="(pages)" options={{ headerShown: false }} />
+				</Stack>
+			</SQLiteProvider>
+		</GestureHandlerRootView >
 	);
 }
 
