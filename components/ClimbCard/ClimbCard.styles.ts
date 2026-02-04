294eba1baca40dec99e726d95124239a5c74d0fd
Tate, Thane <thanetate@my.unt.edu>
2026-02-03 18:32:15 -0600

    Fix

--- components/ClimbCard/ClimbCard.styles.ts
+++ components/ClimbCard/ClimbCard.styles.ts
@@ -1,31 +1,31 @@
 import { Dimensions, StyleSheet } from "react-native";
-import { BORDERRADIUS, COLORS, FONT_SIZES, global } from "../../theme";
+import { global } from "../../theme";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
-  cardContainer: {
-    width: SCREEN_WIDTH - 40,
-    marginBottom: 16,
-    backgroundColor: COLORS.cardBackground,
-    borderRadius: BORDERRADIUS.medium,
-    overflow: "hidden",
-  },
-  media: {
-    width: "100%",
-    height: 180,
-    backgroundColor: global.colors.background_2,
-  },
-  mediaItem: {
-    width: "100%",
-    height: "100%",
-  },
-  info: {
-    padding: 12,
-  },
-  grade: {
-    fontSize: FONT_SIZES.large,
-    fontWeight: "bold",
-    color: global.colors.text_1,
-  },
+	cardContainer: {
+		width: SCREEN_WIDTH - 40,
+		marginBottom: 16,
+		backgroundColor: global.colors.background_2,
+		borderRadius: global.border.border_radius,
+		overflow: "hidden",
+	},
+	media: {
+		width: "100%",
+		height: 180,
+		backgroundColor: global.colors.background_2,
+	},
+	mediaItem: {
+		width: "100%",
+		height: "100%",
+	},
+	info: {
+		padding: 12,
+	},
+	grade: {
+		fontSize: global.font_size.phone_large,
+		fontWeight: "bold",
+		color: global.colors.text_1,
+	},
 });
--- components/RecentClimbs/RecentClimbs.styles.ts
+++ components/RecentClimbs/RecentClimbs.styles.ts
@@ -1,22 +1,22 @@
 import { Dimensions, StyleSheet } from "react-native";
-import { COLORS, FONT_SIZES } from "../../theme";
+import { global } from "../../theme";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
-  section: {
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-  },
-  sectionTitle: {
-    fontSize: FONT_SIZES.medium,
-    fontWeight: "bold",
-    color: COLORS.textGray,
-    marginBottom: 16,
-    letterSpacing: 0.5,
-    fontFamily: "Roboto",
-  },
-  climbsScroll: {
-    width: "100%",
-  },
+	section: {
+		paddingHorizontal: 20,
+		paddingVertical: 20,
+	},
+	sectionTitle: {
+		fontSize: global.font_size.phone_medium,
+		fontWeight: "bold",
+		color: global.colors.text_2,
+		marginBottom: 16,
+		letterSpacing: 0.5,
+		fontFamily: "Roboto",
+	},
+	climbsScroll: {
+		width: "100%",
+	},
 });
